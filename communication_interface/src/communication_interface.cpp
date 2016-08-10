f383edb2e21fa765fc9e301aeb4070f8ff93d681
Wei Shih <willie5588912@gmail.com>
2016-08-10 15:46:27 +0800

    Now add the raw communication_interface.

---
+++ communication_interface/src/communication_interface.cpp
@@ -0,0 +1,17 @@
+#include <communication_interface/communication_interface.h>
+
+void communication_interface::init()
+{
+}
+
+void communication_interface::terminate()
+{
+}
+
+std::vector<int> communication_interface::get_curr_pos()
+{
+}
+
+std::vector<int> communication_interface::update(std::vector<int> jnt_curr_pos_)
+{
+}
