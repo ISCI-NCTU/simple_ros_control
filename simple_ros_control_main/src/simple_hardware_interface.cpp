a101f354a11dec61a57ae0ac691f3e0ebc16206a
Wei Shih <willie5588912@gmail.com>
2016-08-10 10:15:15 +0800

    For Backup.

--- simple_ros_control_main/src/simple_hardware_interface.cpp
+++ simple_ros_control_main/src/simple_hardware_interface.cpp
@@ -2,6 +2,7 @@
 #include <simple_hardware_interface.h>
 
 int count = 0;
+
 SpHwInterface::SpHwInterface(
 	unsigned int m_n_dof_, unsigned int m_update_freq_, 
 	std::vector<std::string> m_jnt_names_, std::vector<float> m_gear_ratios_)
@@ -39,42 +40,6 @@ SpHwInterface::SpHwInterface(
   registerInterface(&jnt_pos_interface_);
 
 }
-#if 0
-SpHwInterface::SpHwInterface():
-    n_dof_(3)
-{
-  // Cleanup
-  jnt_curr_pos_.clear();
-  jnt_curr_vel_.clear();
-  jnt_curr_eff_.clear();
-  jnt_cmd_pos_.clear();
- 
-  // Joints
-  jnt_names_.push_back("joint1");
-  jnt_names_.push_back("joint2");
-  jnt_names_.push_back("joint_eef");
-
-  // Raw Data
-  jnt_curr_pos_.resize(n_dof_);
-  jnt_curr_vel_.resize(n_dof_);
-  jnt_curr_eff_.resize(n_dof_);
-  jnt_cmd_pos_.resize(n_dof_);
-
-  // Hardware Interface
-  for (size_t i = 0; i < n_dof_; ++i)
-  {
-    jnt_state_interface_.registerHandle(
-        hardware_interface::JointStateHandle(jnt_names_[i], &jnt_curr_pos_[i], &jnt_curr_vel_[i], &jnt_curr_eff_[i]));
-
-    jnt_pos_interface_.registerHandle(
-        hardware_interface::JointHandle(jnt_state_interface_.getHandle(jnt_names_[i]), &jnt_cmd_pos_[i]));
-
-    ROS_DEBUG_STREAM("Registered joint '" << jnt_names_[i] << "' in the PositionJointInterface.");
-  }
-  registerInterface(&jnt_state_interface_);
-  registerInterface(&jnt_pos_interface_);
-}
-#endif
 
 SpHwInterface::~SpHwInterface()
 {
@@ -83,7 +48,7 @@ SpHwInterface::~SpHwInterface()
 void SpHwInterface::update()
 {
   // Handle current position (for reading) 
-  for(size_t i = 0; i < NDOF; i++)
+  for(size_t i = 0; i < n_dof_; i++)
     jnt_curr_pos_[i] = jnt_cmd_pos_[i];
 
 #if 1
