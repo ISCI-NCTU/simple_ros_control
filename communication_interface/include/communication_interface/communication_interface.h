0f73a275091e7eb9c850eac91a9272374ff6e8be
Wei Shih <willie5588912@gmail.com>
2016-08-16 16:26:36 +0800

    Now the framework works fine (only check uart).

--- communication_interface/include/communication_interface/communication_interface.h
+++ communication_interface/include/communication_interface/communication_interface.h
@@ -8,8 +8,10 @@ namespace communication_interface
   void init();
   void terminate();
   std::vector<double> get_home_pos();
-  std::vector<double> update(std::vector<double>);
-  std::vector<double> update_vel(std::vector<double>);
+  std::vector<double> update_pp(std::vector<double>);
+  std::vector<double> update_pv(std::vector<double>);
+  std::vector<double> update_vv(std::vector<double>);
+  std::vector<double> update_vp(std::vector<double>);
   void write_cmd_pos(std::vector<double>);
 }
 
